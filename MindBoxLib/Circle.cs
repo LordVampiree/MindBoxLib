9877bf6bb5db34377e128111a660a0a5cf33e126
LordVampiree <76253076+LordVampiree@users.noreply.github.com>
2022-09-17 13:13:02 +0300

    Update Circle.cs

--- MindBoxLib/Circle.cs
+++ MindBoxLib/Circle.cs
@@ -3,12 +3,14 @@
     public class Circle : IFigure
     {
         private readonly double _radius;
+        
         public Circle(double radius)
         {
             _radius = radius;
             if (!IsValid())
                 throw new Exception("Такого круга не существует, неккоректный радиус");
         }
+        
         public double GetSquare() => Math.Round(_radius * _radius * Math.PI, 2);
 
         public bool IsValid() => _radius > 0d;
